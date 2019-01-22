9a4f70408c3d4e522381c5bbad6010617b46fc3a
Mirew <mire.llyta226@gmail.com>
2019-01-22 02:28:59 -0500

    modificado 21/01/2019

---
+++ DiagramaEDT.mm
@@ -0,0 +1,83 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1547771911079" ID="ID_1316044960" MODIFIED="1547777906937" TEXT="BIBLIOTECA">
+<icon BUILTIN="gohome"/>
+<node CREATED="1547772342996" ID="ID_812094711" MODIFIED="1547777983034" POSITION="right" TEXT="Gesti&#xf3;n de Catalogaci&#xf3;n">
+<icon BUILTIN="list"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1547773783827" ID="ID_346350401" MODIFIED="1547777927734" TEXT="Gesti&#xf3;n de Clasificaci&#xf3;n">
+<icon BUILTIN="edit"/>
+</node>
+<node CREATED="1547774018347" ID="ID_1868937320" MODIFIED="1547777935787" TEXT="Gesti&#xf3;n de Tarjetas para Cat&#xe1;logos">
+<icon BUILTIN="edit"/>
+</node>
+<node CREATED="1547774076640" FOLDED="true" ID="ID_1968468384" MODIFIED="1547778039036" TEXT="Gesti&#xf3;n de Baja de Libro">
+<icon BUILTIN="closed"/>
+<node CREATED="1547776867293" ID="ID_1603510497" MODIFIED="1547778007122" TEXT="Deterioro">
+<icon BUILTIN="stop-sign"/>
+</node>
+<node CREATED="1547776871675" ID="ID_995713346" MODIFIED="1547778000570" TEXT="P&#xe9;rdida">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1547774107103" ID="ID_1565039019" MODIFIED="1547778052587" TEXT="Gesti&#xf3;n de Compra">
+<icon BUILTIN="desktop_new"/>
+<node CREATED="1547774269775" ID="ID_1316049798" MODIFIED="1547778057303" TEXT="Lista de Compras">
+<icon BUILTIN="list"/>
+</node>
+<node CREATED="1547774278874" ID="ID_1475300255" MODIFIED="1547778061304" TEXT="Solicitar al Editorial">
+<icon BUILTIN="edit"/>
+</node>
+</node>
+</node>
+<node CREATED="1547772887607" ID="ID_534917812" MODIFIED="1547777971337" POSITION="right" TEXT="Gesti&#xf3;n de &#xc1;rea de Lectura">
+<icon BUILTIN="full-3"/>
+<node CREATED="1547774692874" ID="ID_754603584" MODIFIED="1547778074048" TEXT="Gesti&#xf3;n de Usuarios">
+<icon BUILTIN="group"/>
+<node CREATED="1547774825215" ID="ID_885242838" MODIFIED="1547778272720" TEXT="Inscripci&#xf3;n">
+<icon BUILTIN="attach"/>
+<node CREATED="1547776472099" ID="ID_283174653" MODIFIED="1547778357700" TEXT="Boleta de Inscripci&#xf3;n">
+<icon BUILTIN="info"/>
+</node>
+</node>
+<node CREATED="1547774836607" ID="ID_1792748523" MODIFIED="1547778280252" TEXT="Baja de Usuario">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1547774871914" ID="ID_1743082768" MODIFIED="1547778285251" TEXT="Reclamos">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1547776569082" ID="ID_686362367" MODIFIED="1547778353341" TEXT="Solicitar libro">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1547776660167" ID="ID_1323815779" MODIFIED="1547778348016" TEXT="Solicitar renovaci&#xf3;n">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1547774703437" ID="ID_893937802" MODIFIED="1547778258763" TEXT="Gesti&#xf3;n de Pr&#xe9;stamos">
+<icon BUILTIN="desktop_new"/>
+</node>
+</node>
+<node CREATED="1547772870021" ID="ID_734904559" MODIFIED="1547777974497" POSITION="right" TEXT="Gesti&#xf3;n de Almacen">
+<icon BUILTIN="full-4"/>
+<node CREATED="1547775350298" ID="ID_1418675950" MODIFIED="1547778113333" TEXT="Gesti&#xf3;n de Busqueda de libros">
+<icon BUILTIN="xmag"/>
+</node>
+<node CREATED="1547776750608" ID="ID_1390616737" MODIFIED="1547778181880" TEXT="Gesti&#xf3;n de Servicios Realizados">
+<icon BUILTIN="info"/>
+</node>
+</node>
+<node CREATED="1547773642273" ID="ID_784857304" MODIFIED="1547777963982" POSITION="left" TEXT="Gesti&#xf3;n de Direcci&#xf3;n">
+<icon BUILTIN="folder"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1547774516972" ID="ID_1113364646" MODIFIED="1547777054132" TEXT="Gesti&#xf3;n de Solicitudes">
+<icon BUILTIN="prepare"/>
+</node>
+<node CREATED="1547774538437" ID="ID_1079601223" MODIFIED="1547777045061" TEXT="Gesti&#xf3;n de Autorizaciones">
+<icon BUILTIN="go"/>
+</node>
+<node CREATED="1547774635927" ID="ID_1775821260" MODIFIED="1547777838377" TEXT="Gesti&#xf3;n de Informes">
+<icon BUILTIN="list"/>
+</node>
+</node>
+</node>
+</map>
